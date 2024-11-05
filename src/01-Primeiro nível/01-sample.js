9a4348ce01e42acbcd1a05527025a9ac625d790d
RhyanDaRocha <rhyandarocha20062006@gmail.com>
2024-11-05 12:15:10 -0300

    Adicionando eslint e configurando como projeto node

---
+++ src/01-Primeiro nível/01-sample.js
@@ -0,0 +1,14 @@
+
+function soumteste(a) {
+    let b = [];
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] % 2 === 0) {
+            b.push(a[i]);
+        }
+    }
+    return b;
+}
+
+let x = [1, 2, 3, 4, 5, 6];
+let y = h(x);
+console.log(y);
