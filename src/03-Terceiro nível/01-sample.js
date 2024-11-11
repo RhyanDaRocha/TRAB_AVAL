9652c5321ce1dbe8e4d4045df3aae616377de863
Jeansilva05 <jeanprg074@gmail.com>
2024-11-11 17:40:15 -0300

    :broom: cleanup: Dividindo funções com responsabilidades únicas

--- src/03-Terceiro nível/01-sample.js
+++ src/03-Terceiro nível/01-sample.js
@@ -1,29 +1,42 @@
-async function buscarEDetalharNave() {
-    try {
-        const resposta = await fetch("https://swapi.dev/api/starships/9/");
-        const nave = await resposta.json();
+async function consultarInformacoesDaNave() {
 
-        const detalhesNave = `
+    const nave = await buscarNave();
+    detalharNave(nave);
+    verificarTamanhoDaNave(nave);
+
+}
+
+function detalharNave(nave) {
+    const detalhesNave = `
         Nome: ${nave.name}
         Modelo: ${nave.model}
         Fabricante: ${nave.manufacturer}
         `;
 
-        console.log("Detalhes da Nave:");
-        console.log(detalhesNave);
+    console.log("Detalhes da Nave:");
+    console.log(detalhesNave);
+}
 
-        const tamanhoTripulacao = parseInt(nave.crew);
-        const capacidadeMaximaNavePequena = 100;
+function verificarTamanhoDaNave(nave) {
+    const tamanhoTripulacao = parseInt(nave.crew);
+    const capacidadeMaximaNavePequena = 100;
 
-        if (tamanhoTripulacao > capacidadeMaximaNavePequena) {
-            console.log("Esta é uma nave grande.");
-        } else {
-            console.log("Esta é uma nave pequena.");
-        }
-
-    } catch (erro) {
-        console.error("Erro ao buscar detalhes da nave:", erro);
+    if (tamanhoTripulacao > capacidadeMaximaNavePequena) {
+        console.log("Esta é uma nave grande.");
+    } else {
+        console.log("Esta é uma nave pequena.");
     }
 }
 
-buscarEDetalharNave();
+async function buscarNave() {
+    try {
+        const resposta = await fetch("https://swapi.dev/api/starships/9/");
+        return await resposta.json();
+
+    } catch (erro) {
+        console.error("Erro ao buscar detalhes da nave:", erro);
+        return null;
+    }
+}
+
+consultarInformacoesDaNave();
