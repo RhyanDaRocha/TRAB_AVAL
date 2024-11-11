a1979630ecb36598fe257d227ae4f2af663e0396
Jeansilva05 <jeanprg074@gmail.com>
2024-11-11 18:20:16 -0300

    :broom: cleanup: Dividindo funções com responsabilidades únicas

--- src/03-Terceiro nível/02-sample.js
+++ src/03-Terceiro nível/02-sample.js
@@ -1,20 +1,32 @@
-async function buscarEFiltrarPessoas() {
+function exibirDetalhesDaBusca() {
+    filtrarPessoasPelaPrimeiraLetraDoNome("L");
+    totalDePessoasEncontradas();
+}
+
+async function buscarPessoas() {
     try {
         const resposta = await fetch("https://swapi.dev/api/people/");
-        const dados = await resposta.json();
-
-        const pessoasComL = dados.results.filter(pessoa => pessoa.name.startsWith("L"));
-
-        console.log("Pessoas cujo nome começa com L:");
-        pessoasComL.forEach(pessoa => {
-            console.log(pessoa.name);
-        });
-
-        console.log(`Total de pessoas encontradas: ${dados.results.length}`);
-
+        return await resposta.json();
+        
     } catch (erro) {
         console.error("Erro ao buscar pessoas:", erro);
+        return null;
     }
 }
 
-buscarEFiltrarPessoas();
+async function totalDePessoasEncontradas() {
+    const dados = await buscarPessoas();
+    console.log(`Total de pessoas encontradas: ${dados.results.length}`);
+}
+
+async function filtrarPessoasPelaPrimeiraLetraDoNome(letra) {
+    const dados = await buscarPessoas();
+    const pessoasComALetraDesejada = dados.results.filter(pessoa => pessoa.name.startsWith(letra));
+
+    console.log(`Pessoas cujo nome começa com ${letra}:`);
+    pessoasComALetraDesejada.forEach(pessoa => {
+        console.log(pessoa.name);
+    });
+}
+
+exibirDetalhesDaBusca();
